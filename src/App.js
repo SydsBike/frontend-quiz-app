1303b62232771c54d54a012355b92aa6a4236b6f
Your Name <youremail@example.com>
2025-02-11 09:30:25 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 // App Component
 
+const { useState, useEffect } = React;
+
+
+
+
+
 function App() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenSize(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    //cleanup function
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  console.log(screenSize);
+
   return (
     <>
       <h1>Hello</h1>
+      <img src={assetData.iconAccessibility} />
+      <img src={assetData.iconCorrect} />
+      <img src="../src/assets/images/icon-incorrect.svg" />
     </>
   );
 }
---
+++ src/assets/assetData.js
@@ -0,0 +1,19 @@
+const assetData = {
+  iconAccessibility: "../src/assets/images/icon-accessibility.svg",
+  iconCorrect: "../src/assets/images/icon-correct.svg",
+  iconCss: "../src/assets/images/icon-css.svg",
+  iconError: "../src/assets/images/icon-error.svg",
+  iconHtml: "../src/assets/images/icon-html.svg",
+  iconIncorrect: "../src/assets/images/icon-incorrect.svg",
+  iconJs: "../src/assets/images/icon-js.svg",
+  iconMoonDark: "../src/assets/images/icon-moon-dark.svg",
+  iconMoonLight: "../src/assets/images/icon-moon-light.svg",
+  iconAccessibility: "../src/assets/images/icon-accessibility.svg",
+  patternBackgroundDesktopDark: "../src/assets/images/pattern-background-desktop-dark.svg",
+  patternBackgroundDesktopligh: "../src/assets/images/pattern-background-desktop-light.svg",
+  patternBackgroundMobileDark: "../src/assets/images/pattern-background-mobile-dark.svg",
+  patternBackgroundMobilelight: "../src/assets/images/pattern-background-mobile-light.svg",
+  patternBackgroundTabletDark: "../src/assets/images/pattern-background-tablet-dark.svg",
+  patternBackgroundTabletLight: "../src/assets/images/pattern-background-table-light.svg",
+};
+
